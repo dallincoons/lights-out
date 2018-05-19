070c0f2c5dbb23759eba1b473d69910f39df531b
Dallin <dallinis@hotmail.com>
2018-05-19 16:12:04 -0700

    adds adjacency list

--- src/Graph.js
+++ src/Graph.js
@@ -4,17 +4,20 @@ class Graph {
     verticesCount;
     edgesCount;
     nodes;
+    columns;
+    rows;
 
     constructor(verticesCount, colMax) {
         this.verticesCount = verticesCount;
         this.edgesCount = 0;
         this.nodes  = [];
         this.flatNodes = [];
+        this.adjacent = [];
 
-        const columns = colMax;
-        const rows = Math.ceil(verticesCount / columns);
+        this.columns = colMax;
+        this.rows = Math.ceil(verticesCount / this.columns);
 
-        this.buildGraph(rows, columns, verticesCount);
+        this.buildGraph(this.rows, this.columns, verticesCount);
     }
 
     buildGraph(rows, columns, totalVertices) {
@@ -30,8 +33,41 @@ class Graph {
             for (let j = 0; j < totalLeft; j++) {
                 this.nodes[i][j] = {'coordinates' : {row: i, column:j}, 'isOn' : Math.random() >= 0.5};
                 this.flatNodes.push(this.nodes[i][j]);
+
+                this.addAdjacencies(i , j)
             }
         }
+    }
+
+    addAdjacencies(row, column) {
+        if (! this.adjacent[row]) {
+            this.adjacent[row] = {};
+        }
+
+        let adjacentList = [];
+
+        if(column < this.columns - 1) {
+            adjacentList.push({row : row, column: column + 1});
+        }
+
+        if(row < this.rows - 1) {
+            adjacentList.push({row : row + 1, column: column});
+        }
+
+        if(row !== 0) {
+            adjacentList.push({row: row - 1, column: column});
+        }
+
+        if(column !== 0) {
+            adjacentList.push({row: row, column: column - 1});
+        }
+
+        this.adjacent[row][column] = adjacentList;
+
+    }
+
+    adjacentNodes(coordinates) {
+        return this.adjacent[coordinates.row][coordinates.column];
     }
 
     listVertices() {
--- src/Graph.test.js
+++ src/Graph.test.js
@@ -25,5 +25,29 @@ it('renders grid with 10 elements and 2 columns', () => {
 it('gets list of vertices', () => {
     let graph = new Graph(2, 2);
 
-    console.log(graph.listVertices());
+    // console.log(graph.listVertices());
+});
+
+it('it gets adjacent node when node is at the top left', () => {
+    let graph = new Graph(25, 5);
+
+    let adjacent = graph.adjacentNodes({row: 0, column: 0});
+
+    expect(adjacent).toEqual([{row: 0, column: 1}, {row: 1, column: 0}]);
+});
+
+it('it gets adjacent nodes for middle node', () => {
+    let graph = new Graph(25, 5);
+
+    let adjacent = graph.adjacentNodes({row: 1, column: 1});
+
+    expect(adjacent).toEqual([{column: 2, row: 1}, {column: 1, row: 2}, {column: 1, row: 0}, {column: 0, row: 1}]);
+});
+
+it('it gets adjacent nodes for bottom right', () => {
+    let graph = new Graph(25, 5);
+
+    let adjacent = graph.adjacentNodes({row: 4, column: 4});
+
+    expect(adjacent).toEqual([{column: 4, row: 3}, {column: 3, row: 4}]);
 });
